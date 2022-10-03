2b560947d78cd5b1a549469ee6c7f89e295bb9e7
Beulah Mathenge <beulahmathenge@gmail.com>
2022-10-03 22:08:24 +0300

    Index JS and App Js setup for rendering app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import NavBarMenu from "./Components/NavBarMenu"
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Blogs from "./Pages/Blogs";
+import Contact from "./Pages/Contact";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+       <NavBarMenu /> 
+       <Switch>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/blogs'>
+          <Blogs />
+        </Route>
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+       </Switch>     
+    </Router>
+    
+    </>
   );
 }
 
