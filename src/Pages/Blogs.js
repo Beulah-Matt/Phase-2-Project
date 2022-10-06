afe98c0e751f37eef96e70fcff5529ac19e8b393
Beulah Mathenge <beulahmathenge@gmail.com>
2022-10-06 10:21:00 +0300

    Updating the blogs page, heading and subheading

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import BlogsSection from '../Components/BlogsSection'
+import styled from 'styled-components'
+import Titles from '../Components/Titles'
+//import BlogsSection from '../Components/BlogsSection'
+
+const BlogStyles = styled.div
+`
+padding: 10rem 0;
+
+`;
 
 export default function Blogs() {
   return (
-    <div>
-       <BlogsSection />
-    </div>
+    <BlogStyles>
+      <div className="container">
+        <Titles title='About Me' subtitle='Get to know me'/> 
+      </div>
+    </BlogStyles>
   )
 }
