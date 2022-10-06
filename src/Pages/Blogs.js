dc3f1763226948fa4ef253b9cae26648a1a03e52
Beulah Mathenge <beulahmathenge@gmail.com>
2022-10-06 11:38:55 +0300

    Styled search bar and input area

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,19 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdSearch } from 'react-icons/md';
 import styled from 'styled-components'
+// import BlogItem from '../Components/BlogItem';
 import Titles from '../Components/Titles'
-//import BlogsSection from '../Components/BlogsSection'
+// import BlogsSection from '../Components/BlogsSection'
 
 const BlogStyles = styled.div
 `
 padding: 10rem 0;
+.blogSearchBar {
+  position: relative;
+  width: 300px;
+  margin-top: 5rem;
+}
+.blogSearchBar input {
+  width: 100%;
+  font-size: 2rem;
+  padding: 0.8rem;
+  color: var(--black);
+  border-radius: 6px;
+  outline: none;
+  border: none;
+}
+.blogSearchBar .searchIcon {
+  position: absolute;
+  width: 2rem;
+  right: 1rem;
+}
+.blogSearchBar .searchIcon path {
+  color: #7393B3 ;
+}
 
 `;
 
 export default function Blogs() {
+  // Loop through all blog items and display them here using the Blog item component
+  //Add search functionality to this page
+
+const [search, setSearch] = useState('');
+
   return (
     <BlogStyles>
       <div className="container">
-        <Titles title='About Me' subtitle='Get to know me'/> 
+        <Titles title='Articles' subtitle='Enjoy reading this'/> 
+        <div className="blogSearchBar">
+          <form>
+            <input type="text" 
+            
+            />
+            <MdSearch className='searchIcon'/>
+          </form>
+        </div>
+        <div className="allBlogs">
+
+          {/* <BlogItem /> */}
+          {/* <BlogsSection /> */}
+        </div>
       </div>
     </BlogStyles>
   )
